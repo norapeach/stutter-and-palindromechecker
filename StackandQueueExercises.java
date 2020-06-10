f959bb8a51039e44bb1af83d205b540b4e7063d3
Nora P <veganmuffcakes@gmail.com>
2020-06-09 22:37:35 -0700

    Incomplete version: adds logic for isPalindrome in comments, start of implementation

--- StackandQueueExercises.java
+++ StackandQueueExercises.java
@@ -7,7 +7,7 @@ import java.util.Stack;
  */
 
 /***************************************************************************
- * BJP5 Exercise 14.2: stutter
+ * START LINE 82: BJP5 Exercise 14.2: stutter
  * This program tests the method stutter that takes a stack of 
  * integers as a parameter and replaces every value in the stack 
  * with two occurrences of that value.
@@ -21,6 +21,7 @@ import java.util.Stack;
  * "In the original list the 9 was at the top and would have been popped first. 
  * In the new stack the two 9s would be the first values popped from the stack." 
  **************************************************************************/
+
 /***************************************************************************
  * BJP5 Exercise 14.8: isPalindrome
  * This program also tests the method a method isPalindrome: 
@@ -57,47 +58,131 @@ public class StackandQueueExercises {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Stack<Integer> s = new Stack<>();
-		int[] nums1 = {3, 7, 1, 14, 9};
-		
-		
-		for (int n : nums1) {
+		Stack<Integer> s = new Stack<>(); // for stutter
+		Queue<Integer> testPal1 = new LinkedList<>();
+		Queue<Integer> testPal2 = new LinkedList<>();
+		int[] stutter1 = { 3, 7, 1, 14, 9 }; // test for stutter
+		int[] pal1 = {3, 8, 17, 9, 17, 8, 3}; // test for palindrome
+		int[] pal2 = {3, 8, 17, 9, 4, 17, 8, 3}; // !palindrome
+
+		for (int n : stutter1) {
 			s.push(n);
 		}
-		System.out.println("Original Stack: bottom " + s + " top ");
+		
+		System.out.println("Stutter Test:");
+		System.out.println("Original Stack: bottom " + s + " top \n");
 		stutter(s);
-		System.out.println("Stuttered Stack: bottom " + s + " top ");
+		System.out.println("Stuttered Stack: bottom " + s + " top \n");
+		
+		for (int n : pal1) {
+			testPal1.add(n);
+		}
+		
+		for (int n : pal2) {
+			testPal2.add(n);
+		}
+		
+		System.out.println("isPalindrome Test:");
+		System.out.println("Original Queue 5: " + testPal1);
+		System.out.println("Palindrome? >> " + isPalindrome(testPal1) + " \n");
+		System.out.println("Original Queue 6: " + testPal2);
+		System.out.println("Palindrome? >> " + isPalindrome(testPal2) + " \n");
 
 	}
-	
+
 	/**
-	 * Duplicates each integer in the stack, maintaining the original order
-	 * Uses 1 Queue as auxiliary storage.
+	 * Duplicates each integer in the stack, maintaining the original order Uses 1
+	 * Queue as auxiliary storage.
+	 * 
 	 * @param s The stack of integers to stutter
 	 */
 	public static void stutter(Stack<Integer> s) {
 		Queue<Integer> q = new LinkedList<>();
-		while(!s.isEmpty()) { // loop through stack - post: all values will be removed and added to q
+		while (!s.isEmpty()) { // loop through stack - post: all values will be removed and added to q
 			int n = s.pop();
 			q.add(n);
 		}
-		
-		while(!q.isEmpty()) {
+
+		while (!q.isEmpty()) {
 			int n = q.remove();
 			s.push(n);
 		} // post: Stack is reversed
-		
-		while(!s.isEmpty()) {
+
+		while (!s.isEmpty()) {
 			int n = s.pop();
 			q.add(n);
-		} 
-		
+		}
+
 		while (!q.isEmpty()) {
 			int n = q.remove();
 			s.push(n);
 			s.push(n);
 		} // post: correct original order, but stuttered
-		
+
 	}
+
+	/**
+	 * Returns true if the given Queue reads the same forwards as backwards.
+	 * Trivially true for empty or 1-letter strings.
+	 * 
+	 * @param q The queue to evaluate as in a palindrome sequence
+	 * @return boolean
+	 */
+	public static boolean isPalindrome(Queue<Integer> q) {
+		// I want to get the reverse and compare each int from the queue and stack
+		// two tracks?
+		// WHAT DO I KNOW FIRST: trying recursive solutation
+
+		int origSize = q.size(); // get the queue size
+		Stack<Integer> s = new Stack<>(); // REQUIREMENT: 1 stack as aux storage
+
+		if (origSize < 2) {
+			return true; // base case
+		} else {
+			while (!q.isEmpty()) {
+				int n = q.remove(); // remove all values from queue
+				s.push(n); // add to stack
+			} //1. post: Temp Stack contains original sequence
+			System.out.println("tracing... " + "temp stack: " + " bottom " + s + " top");
+			System.out.println("tracing... " + "queue: front " + q +  " back\n");
+		
+			while (!s.isEmpty()) {
+				int n = s.pop();
+				q.add(n);
+			} // 2. post: queue contains reversed
+		
+			// compare?
+		} 
+		
+		
+		
+		
+		
+		
+
+		// REQUIREMENT: restore the queue
+		return false;
+	}
+	// REFERENCE googledoc: npRecursion basic examples --> search isPalindrome
+	/***************************************************************************
+	 * BJP5 Exercise 14.8: isPalindrome This program also tests the method a method
+	 * isPalindrome: "Take a queue of integers as a parameter and return true if the
+	 * numbers in the queue represent a palindrome (and false otherwise). A sequence
+	 * of numbers is considered a palindrome if it is the same in reverse order. For
+	 * example, suppose a queue called q stores these values: front [3, 8, 17, 9,
+	 * 17, 8, 3] back isPalindrome(q); should return true because this sequence is
+	 * the same in reverse order.
+	 * 
+	 * If the queue had instead stored these values: front [3, 8, 17, 9, 4, 17, 8,
+	 * 3] back The call on isPalindrome would instead return false because this
+	 * sequence is not the same in reverse order (the 9 and 4 in the middle don't
+	 * match).
+	 * 
+	 * REQUIREMENTS - The empty queue should be considered a palindrome. - You may
+	 * not make any assumptions about how many elements are in the queue and your
+	 * method must restore the queue so that it stores the same sequence of values
+	 * after the call as it did before. - You may use one stack as auxiliary
+	 * storage.
+	 **************************************************************************/
 
 }
